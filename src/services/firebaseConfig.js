39b970c0dd2021ea74613e1111cb352dc1e6b4d9
Nicolas Alberto <naranonva@hotmail.com>
2022-04-30 19:05:11 -0300

    Adicionado link para o realtimedatabase

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -5,6 +5,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "marea-pi05.firebaseapp.com",
+    databaseURL: "marea-pi05-default-rtdb.firebaseio.com",
     projectId: "marea-pi05",
     storageBucket: "marea-pi05.appspot.com",
     messagingSenderId: "442681379300",
