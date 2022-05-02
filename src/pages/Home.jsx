097ee905caa12f34f2bcf6e4cc2089c4728042f0
CelticSlash <henriquepereiralima@yahoo.com.br>
2022-05-01 23:15:20 -0300

    Mockup listagem de veiculos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,61 @@ const Home = () => {
                     <Helmet>
                         <title>Home</title>
                     </Helmet>
-                    HOMEEEEEEEEEEEEEEEEEEEEEEE
+                    <div className='row'>
+                        <div className='col-2'>
+                            <div className='container bg-white shadow p-3 mt-3'>
+                                <span>filtros</span>
+                            </div>
+                        </div>
+                        <div className='col-10'>
+                            <div className='row'>
+                                <div className='container bg-white shadow p-3 m-3' style={{'max-width' : '330px'}}>
+                                    <img src='https://via.placeholder.com/300x200' className='d-block'/>
+                                    <span className='d-block'>Nome da jabiraca</span>
+                                    <span className='d-block'>R$ 25.000,00</span>
+                                    <span className='text-muted pe-2'>Flex</span>
+                                    <span className='text-muted pe-2'>Manual</span>
+                                    <span className='text-muted pe-2'>Ar Condicionado</span>
+                                </div>
+
+                                <div className='container bg-white shadow p-3 m-3' style={{'max-width' : '330px'}}>
+                                    <img src='https://via.placeholder.com/300x200' className='d-block'/>
+                                    <span className='d-block'>Nome da jabiraca</span>
+                                    <span className='d-block'>R$ 25.000,00</span>
+                                    <span className='text-muted pe-2'>Flex</span>
+                                    <span className='text-muted pe-2'>Manual</span>
+                                    <span className='text-muted pe-2'>Ar Condicionado</span>
+                                </div>
+
+                                <div className='container bg-white shadow p-3 m-3' style={{'max-width' : '330px'}}>
+                                    <img src='https://via.placeholder.com/300x200' className='d-block'/>
+                                    <span className='d-block'>Nome da jabiraca</span>
+                                    <span className='d-block'>R$ 25.000,00</span>
+                                    <span className='text-muted pe-2'>Flex</span>
+                                    <span className='text-muted pe-2'>Manual</span>
+                                    <span className='text-muted pe-2'>Ar Condicionado</span>
+                                </div>
+
+                                <div className='container bg-white shadow p-3 m-3' style={{'max-width' : '330px'}}>
+                                    <img src='https://via.placeholder.com/300x200' className='d-block'/>
+                                    <span className='d-block'>Nome da jabiraca</span>
+                                    <span className='d-block'>R$ 25.000,00</span>
+                                    <span className='text-muted pe-2'>Flex</span>
+                                    <span className='text-muted pe-2'>Manual</span>
+                                    <span className='text-muted pe-2'>Ar Condicionado</span>
+                                </div>
+
+                                <div className='container bg-white shadow p-3 m-3' style={{'max-width' : '330px'}}>
+                                    <img src='https://via.placeholder.com/300x200' className='d-block'/>
+                                    <span className='d-block'>Nome da jabiraca</span>
+                                    <span className='d-block'>R$ 25.000,00</span>
+                                    <span className='text-muted pe-2'>Flex</span>
+                                    <span className='text-muted pe-2'>Manual</span>
+                                    <span className='text-muted pe-2'>Ar Condicionado</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                 </Container>
             </Grid>
