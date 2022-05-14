30e97600e74532322287821809514d1ac11f3ac8
Denílson Pereira <deene67@gmail.com>
2022-05-14 12:33:48 -0300

    ajuste firebase config

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY_FIREBASE,
+    apiKey: process.env.FIREBASE_KEY,
     authDomain: "marea-pi05.firebaseapp.com",
     databaseURL: "marea-pi05-default-rtdb.firebaseio.com",
     projectId: "marea-pi05",
     storageBucket: "marea-pi05.appspot.com",
     messagingSenderId: "442681379300",
-    appId: process.env.API_ID_FIREBASE
+    appId: process.env.FIREBASE_ID
 };
 
 export const app = initializeApp(firebaseConfig);
