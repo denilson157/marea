7ed169f89d5ce858c2d54f6d541f0cc0c05c9222
Denílson Pereira <deene67@gmail.com>
2022-05-14 12:00:54 -0300

    att nome api key

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
+    apiKey: process.env.API_KEY_FIREBASE,
     authDomain: "marea-pi05.firebaseapp.com",
     databaseURL: "marea-pi05-default-rtdb.firebaseio.com",
     projectId: "marea-pi05",
     storageBucket: "marea-pi05.appspot.com",
     messagingSenderId: "442681379300",
-    appId: process.env.REACT_APP_API_ID
+    appId: process.env.API_ID_FIREBASE
 };
 
 export const app = initializeApp(firebaseConfig);
